03214a8095b4c211ea5788ce996b8c802f8a431d
priyanka-0605 <149793516+priyanka-0605@users.noreply.github.com>
2024-09-10 18:16:07 +0530

    Add files via upload
    pangram.python

---
+++ pangram.py
@@ -0,0 +1,31 @@
+# print("helllo world")
+# # list=[4,6,5]
+# # for i in range(len(list)):
+# #     for j in range(list[i]):
+# #         print("*",end=" ")
+# #     print()
+
+
+
+
+# # list=[]
+# # pangram=True
+# # sentence="The quick brown fox jumps over the lazy dog"
+# # for i in range(97,123):
+# #     list.append(chr(i))
+# # for i in list:
+# #     if i not in sentence:
+# #         pangram=False
+# # if pangram==True:
+# #     print("String is pangram")
+# # else:
+# #     print("Not a pangram")
+
+
+
+
+
+
+    
+
+
